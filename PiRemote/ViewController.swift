5b37ff829ffd2407f37e42d44b267129080b17ab
Beth Schairer <NoahSchairer@noahs-macbook-pro.local>
2018-03-01 22:51:15 -0500

    Finished

--- PiRemote/ViewController.swift
+++ PiRemote/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -15,11 +16,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func forward(_ sender: Any) {
+        Alamofire.request("http://IPADDRESS:PORT/forward").responseString { (printed) in
+            let result = printed.description
+            print(result)
+        }
     }
+    @IBAction func backward(_ sender: Any) {
+        Alamofire.request("http://IPADDRESS:PORT/backward").responseString { (printed) in
+            let result = printed.description
+            print(result)
+        }
+    }
+    @IBAction func left(_ sender: Any) {
+        Alamofire.request("http://IPADDRESS:PORT/left").responseString { (printed) in
+            let result = printed.description
+            print(result)
+        }
+    }
+    @IBAction func right(_ sender: Any) {
+        Alamofire.request("http://IPADDRESS:PORT/right").responseString { (printed) in
+            let result = printed.description
+            print(result)
+        }
+    }
+    @IBAction func stop(_ sender: Any) {
+        Alamofire.request("http://IPADDRESS:PORT/stop").responseString { (printed) in
+            let result = printed.description
+            print(result)
+        }
 
 
+}
 }
 
